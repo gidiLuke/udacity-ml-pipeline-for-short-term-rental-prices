37f79f834ffa8c7bee87f7244fe9eb470def384d
Lukas Heidegger <lukas.heidegger@eon.com>
2024-02-12 17:24:58 +0100

    Trained model

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 """
-Performs basic cleaning on the data and save the results in Weights & Biases
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
+import pandas as pd
 import wandb
 
 
@@ -11,47 +12,67 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
-def go(args):
-
+def go(args) -> None:
+    logger.info("Starting wandb run for basic cleaning.")
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info("Using artifact: %s", args.input_artifact)
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(filepath_or_buffer=artifact_local_path)
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # Drop outliers
+    logger.info(
+        "Dropping outliers outside of $%.2f and $%.2f.", args.min_price, args.max_price
+    )
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df["price"].between(left=min_price, right=max_price)
+    df = df[idx].copy()
+
+    # Convert last_review to datetime
+    logger.info("Converting 'last_review' column to datetime.")
+    df["last_review"] = pd.to_datetime(arg=df["last_review"])
+
+    df.to_csv(path_or_buf="clean_sample.csv", index=False)
+    logger.info("Cleaned data saved to 'clean_sample.csv'.")
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(local_path="clean_sample.csv")
+    run.log_artifact(artifact)
+    logger.info(
+        "Artifact %s of type %s logged.", args.output_artifact, args.output_type
+    )
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This steps cleans the data")
-
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--input_artifact", type=str, help="Input artifact", required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--output_artifact", type=str, help="Output artifact", required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--output_type", type=str, help="Type of the output", required=True
     )
 
+    parser.add_argument(
+        "--output_description", type=str, help="Output Description", required=True
+    )
+
+    parser.add_argument("--min_price", type=int, help="Min price", required=True)
+
+    parser.add_argument("--max_price", type=int, help="Max Price", required=True)
 
     args = parser.parse_args()
 
-    go(args)
+    go(args=args)
